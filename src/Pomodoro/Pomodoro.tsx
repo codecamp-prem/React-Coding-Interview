174fc574f8cbe746aa45ee4aa2d9daa6a12ce458
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-12-22 12:13:55 +0545

    pomodoro timer

--- src/Pomodoro/Pomodoro.tsx
+++ src/Pomodoro/Pomodoro.tsx
@@ -1,23 +1,70 @@
+import { useRef, useState } from "react";
 import "./pomodoro.css";
 
+// teaches useRef importance in React JS
 const Pomodoro = () => {
+  const [title, setTitle] = useState("let the countdown begin!");
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const intervalRef = useRef(0);
+
+  const startTimer = () => {
+    if (intervalRef.current !== 0) return;
+
+    setTitle("Pomodoro mode!!");
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) return timeLeft - 1;
+
+        resetTimer();
+        return 0;
+      });
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    if (intervalRef.current === 0) return;
+
+    clearInterval(intervalRef.current);
+    setTitle("Just do it!! Keep going!!");
+    setIsRunning(false);
+    intervalRef.current = 0;
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    setTitle("let the countdown begin!");
+    setTimeLeft(25 * 60);
+    setIsRunning(false);
+    intervalRef.current = 0;
+  };
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft - minutes * 60;
+
   return (
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{title}</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{padTime(minutes)}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{padTime(seconds)}</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
 };
 
+function padTime(time: number) {
+  // add 0 to single digit number like 1 to 01
+  return time.toString().padStart(2, "0");
+}
 export default Pomodoro;
