a2408e98b59e2bf5fbb042d5d3a62a6d133049ea
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-12-24 15:11:51 +0545

    tab highlight on hover

--- src/Tab/Tab.tsx
+++ src/Tab/Tab.tsx
@@ -1,19 +1,53 @@
+import { ReactNode, useState } from "react";
 import "./tab.css";
+
+const SingleTab = ({ children }: { children: ReactNode }) => {
+  const [highlightStyle, setHighlightStyle] = useState({ left: 0, opacity: 0 });
+
+  const handleMouseMoveTab = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    setHighlightStyle({
+      left: e.nativeEvent.offsetX - 150,
+      opacity: 0.7,
+    });
+  };
+
+  const handleMouseOutTab = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    setHighlightStyle({
+      left: e.nativeEvent.offsetX - 150,
+      opacity: 0,
+    });
+  };
+
+  return (
+    <div
+      className="tab"
+      onMouseMove={handleMouseMoveTab}
+      onMouseOut={handleMouseOutTab}
+    >
+      <div className="highlight" style={highlightStyle}></div>
+      {children}
+    </div>
+  );
+};
 
 const Tab = () => {
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
+          <SingleTab>
             <a>Home</a>
-          </div>
-          <div className="tab">
+          </SingleTab>
+          <SingleTab>
             <a>About</a>
-          </div>
-          <div className="tab">
+          </SingleTab>
+          <SingleTab>
             <a>Features</a>
-          </div>
+          </SingleTab>
         </div>
         <div className="viewport">Pages Go here</div>
       </div>
@@ -22,3 +56,7 @@ const Tab = () => {
 };
 
 export default Tab;
+
+// React Skills for this App
+// - React Router
+// - Mouse Event
