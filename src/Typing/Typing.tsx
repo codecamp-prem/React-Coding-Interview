47f7ee30f4e7c8e16f325cc12c525911acb28232
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-12-26 12:44:22 +0545

    finding wrong typed character

--- src/Typing/Typing.tsx
+++ src/Typing/Typing.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import "./typing.css";
 
-const secondsToCount = 10;
-const paragraph = `Coding is the best. We are able to build something from scratch. It is literally imagination incarnate. Solving our own problems through coding is one of the coolest things we could do!`;
+const paragraph = `React lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps.`;
+
+const findDiffTypos = (str1: string, str2: string) => {
+  let typos: number[] = [];
+  str2.split("").forEach((character, index) => {
+    if (character !== str1.charAt(index)) {
+      typos.push(index);
+    }
+  });
+  return typos;
+};
 
 const Typing = () => {
+  const [typedText, setTypedText] = useState("");
+  const [typoIndexArray, setTypoIndex] = useState<number[]>([]);
+
+  const handleTypedText = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
+    setTypedText(e.target.value);
+
+  useEffect(() => {
+    setTypoIndex(findDiffTypos(paragraph, typedText));
+  }, [typedText]);
+
   return (
     <div className="app">
       {/* sidebar */}
@@ -15,11 +35,32 @@ const Typing = () => {
 
       <div className="content">
         {/* show the paragraph */}
-        <p>{paragraph}</p>
+        <p>
+          {paragraph.split("").map((character, index) => {
+            let characterClass = "";
+            const hasBeenType = typedText.length > index;
+            if (hasBeenType) {
+              let typo = typoIndexArray.includes(index);
+              if (typo) characterClass = "incorrect";
+              if (!typo) characterClass = "correct";
+            }
+
+            return (
+              <span key={index} className={characterClass}>
+                {character}
+              </span>
+            );
+          })}
+        </p>
 
         {/* show the textarea */}
         <form>
-          <textarea rows={10} placeholder="Test your typing skills..." />
+          <textarea
+            rows={10}
+            placeholder="Test your typing skills..."
+            value={typedText}
+            onChange={handleTypedText}
+          />
         </form>
       </div>
     </div>
