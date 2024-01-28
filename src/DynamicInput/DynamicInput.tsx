20fa38d81e1d4dbc6febe5a2232451f3b01168ad
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-01-28 18:17:41 +0545

    using forwardRef with typescript

--- src/DynamicInput/DynamicInput.tsx
+++ src/DynamicInput/DynamicInput.tsx
@@ -1,13 +1,17 @@
+import { useRef } from "react";
 import "../index.css";
 import Button from "./Button";
 import Container from "./Container";
 import Input from "./Input";
 
 const DynamicInput = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+
   return (
     <main className="w-11/12 max-w-[40rem] m-1 mx-auto p-4 bg-[#3a4346] text-[#f0f6f8] border-2 rounded-md shadow-slate-800">
-      <Input label="Name" id="name" type="text" />
-      <Input label="Age" id="age" type="number" />
+      <Input label="Name" id="name" type="text" ref={nameRef} />
+      <Input label="Age" id="age" type="number" ref={ageRef} />
       <p className="flex space-x-3 m-4">
         {/*ComponentPropsWithoutRef only allows attribute set in it's Generic type
         // suppose passing href in button will be error.*/}
