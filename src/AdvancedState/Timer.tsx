a07d3f934ecd9c0fbe62d7e79670dbe1d2f98ec7
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-01-29 16:02:55 +0545

    useEffect with typescript

--- src/AdvancedState/Timer.tsx
+++ src/AdvancedState/Timer.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import Container from "../DynamicInput/Container";
+import "./progressBar.css";
 import { Timer as TimerProps } from "./store/timers-context";
-
 const Timer = ({ name, duration }: TimerProps) => {
+  const [remainingTime, SetRemaninigTime] = useState(duration * 100);
+  useEffect(() => {
+    setInterval(function () {
+      SetRemaninigTime((prevTime) => prevTime - 50);
+    }, 50);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   return (
     <Container as="article">
       <h2 className="text-4xl text-center m-0">{name}</h2>
-      <p className="text-center text-3xl text-white">{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p className="text-center text-3xl text-black">
+        {formattedRemainingTime}
+      </p>
     </Container>
   );
 };
