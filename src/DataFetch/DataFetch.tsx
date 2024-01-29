6f936457b30876c8f6626c0bfc414722e6e37e46
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-01-29 16:42:14 +0545

    create some utility function to fetch data and use it

--- src/DataFetch/DataFetch.tsx
+++ src/DataFetch/DataFetch.tsx
@@ -1,6 +1,9 @@
 // Data Fetch in TypeScript
 
+import { get } from "./utils/http";
+
 const DataFetch = () => {
+  const posts = get("https://jsonplaceholder.typicode.com/posts");
   return <h1>DataFetch</h1>;
 };
 
