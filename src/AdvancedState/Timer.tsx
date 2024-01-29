44b4a72f88c684513b568fef7b9a36643e27b074
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-01-29 16:25:05 +0545

    add stop/start functionality

--- src/AdvancedState/Timer.tsx
+++ src/AdvancedState/Timer.tsx
@@ -1,24 +1,35 @@
 import { useEffect, useRef, useState } from "react";
 import Container from "../DynamicInput/Container";
 import "./progressBar.css";
-import { Timer as TimerProps } from "./store/timers-context";
+import { Timer as TimerProps, useTimersContext } from "./store/timers-context";
+
 const Timer = ({ name, duration }: TimerProps) => {
   const intervalRef = useRef<number | null>(null);
 
   const [remainingTime, SetRemaninigTime] = useState(duration * 1000);
 
+  const { isRunning } = useTimersContext();
+
   if (remainingTime <= 0 && intervalRef.current) {
+    // timer got down to 0:00
     clearInterval(intervalRef.current);
   }
-  useEffect(() => {
-    const timer = setInterval(function () {
-      SetRemaninigTime((prevTime) => prevTime - 50);
-    }, 50);
 
-    intervalRef.current = timer;
+  useEffect(() => {
+    let timer: number;
+
+    if (isRunning) {
+      timer = setInterval(function () {
+        SetRemaninigTime((prevTime) => prevTime - 50);
+      }, 50);
+
+      intervalRef.current = timer;
+    } else if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
 
     return () => clearInterval(timer);
-  }, []);
+  }, [isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   return (
